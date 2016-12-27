aa9052143a99b053c29561321a7deda45ac9e98b
Jonathan Navarrete <jonatns@gmail.com>
2016-12-27 17:56:49 -0400

    Added / route

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+  res.status(200).send({message: 'welcome'});
+});
+
 app.listen(3000, () => console.log("Server running on port 3000"));
